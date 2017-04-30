f1b08bdf886fbb11771ab7c0975bd099e973ec3c
Elio Acar <xx13nrvxx@gmail.com>
2017-04-30 16:19:19 +0200

    Final version of design
    Fully designed

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-function blink(ob) {
+/*<!--function blink(ob) {
     if (ob.style.visibility == "visible") {
         ob.style.visibility = "hidden";
     } else {
         ob.style.visibility = "visible";
     }
 }
-setInterval("blink(bl)", 500);
+setInterval("blink(bl)", 500);*/
+function init()
+{
+    document.getElementById("decryptedarea").focus();
+}
+
